0ab03ab5cc38c9c8a306a6859cdcabb83acbede9
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-04-28 20:58:48 -0500

    feat :sparkles: advance in sunrise ans set

--- script/script.js
+++ script/script.js
@@ -1,18 +1,19 @@
 function partesuperior(){
     let ciudad = "Floridablanca";
     let codigo = "ddd71d032c0b41bdabd115758252804";
-    let info = `http://api.weatherapi.com/v1/current.json?key=${codigo}&q=${ciudad}`;
+    let info = `http://api.weatherapi.com/v1/current.json?key=${codigo}&q=${ciudad}=es&days=14`;
     let clima = new XMLHttpRequest();
     const imagenes = document.getElementById("Seccion1__ParteSuperior--api");
     
     clima.open("GET", info, true);
     clima.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            let response = JSON.parse(this.responseText);  
+            let response = JSON.parse(this.responseText); 
+
             imagenes.innerHTML = `
                 <p class="location">${response.location.name}, ${response.location.country}</p>
                 <p class="KindOfWeather">${response.current.condition.text}</p>
-                <p class="date">${response.current.last_updated}</p>
+                <p class="date">${response.location.localtime}</p>
                 <img class="Image1" src="${response.current.condition.icon}"/>
                 <p class="temperature">${response.current.temp_c} º</p>
                 <p class="temperatureSensation">Feels like ${response.current.feelslike_c} º</p>
@@ -32,7 +33,7 @@ partesuperior();
 function partetabla(){
     let ciudad = "Floridablanca";
     let codigo = "ddd71d032c0b41bdabd115758252804";
-    let info = `http://api.weatherapi.com/v1/current.json?key=${codigo}&q=${ciudad}`;
+    let info = `http://api.weatherapi.com/v1/current.json?key=${codigo}&q=${ciudad}=es&days=14`;
     let clima = new XMLHttpRequest();
     const imagenes = document.getElementById("Seccion1__Cuadricula--api");
     
@@ -53,3 +54,28 @@ function partetabla(){
 }
 
 partetabla();
+
+function sun(){
+    let ciudad = "Floridablanca";
+    let codigo = "ddd71d032c0b41bdabd115758252804";
+    let info = `https://api.weatherapi.com/v1/forecast.json?key=${codigo}&q=${ciudad}&lang=es&days=14`
+    let clima = new XMLHttpRequest();
+    const imagenes = document.getElementById("Seccion1__sun--api");
+    
+    clima.open("GET", info, true);
+    clima.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.responseText);  
+            imagenes.innerHTML = `
+                <p class="sunset">${responseforecast.forecastday[0].astro.sunset}</p>
+                <p class="sunrise">${responseforecast.forecastday[0].astro.sunrise}</p>
+                <p class="hoursrise">${response.current.pressure_mb} hpa</p>
+                <p class="hoursset">${response.current.uv}</p>
+            `;
+            console.log(response.location.name);
+        }
+    }
+    clima.send();
+}
+
+sun();
